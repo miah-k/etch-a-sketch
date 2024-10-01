25352b8391c1376cb15b51e39131b6c35415dd3b
miah-k <miahkirton2@gmail.com>
2024-10-01 18:28:10 -0400

    Add etch-a-sketch functionality
    Get mouse to highlight background of individual div, of the  16x16 grid, on hover using an event listener.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 
+let offset = 0; 
 for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         const divSquare = document.createElement('div'); 
         divSquare.style.height= "8px";
         divSquare.style.width = "8px"; 
-        divSquare.style.backgroundColor = "gray";  
+        divSquare.style.backgroundColor = "yellow";  
+        divSquare.id =i+offset; 
         document.getElementById("container").appendChild(divSquare); 
+        offset++; 
     }
 }
 
+function checkHover(){
+    const divContainer = document.querySelector("#container"); 
+    divContainer.addEventListener("mouseover", function(e){
+        let myDiv = document.getElementById(e.target.id); 
+        myDiv.style.backgroundColor="black"; 
+    }); 
+}
+
+checkHover(); 
